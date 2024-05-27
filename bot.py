d2cbb45d08cc1a4557a5460849073510852b361f
Ivan Markevich <48765860+g1potez@users.noreply.github.com>
2024-05-27 15:17:48 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,25 @@
 import telebot
 import datetime
 
-bot = telebot.TeleBot('your token', parse_mode=None)
-enemy_text = 'youtube.com/shorts'
+bot = telebot.TeleBot('TOKEN', parse_mode=None)
+enemy_text = 'youtube.com'
+edit_text = 'youtu.be'
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, "Система 'Анти шортсы' - активировано!")
+	bot.send_message(message.chat.id, "Система 'ANTI SHORTS' - активировано!")
+
+@bot.message_handler(commands=['stop'])
+def send_welcome(message):
+	bot.send_message(message.chat.id, "Ты долбаёб?")
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+	user_name = message.from_user.username
+	if user_name == 'YOUR USERNAME':
+		bot.send_message(message.chat.id, "Всё что угодно!")
+	else:
+		bot.send_message(message.chat.id, f"{user_name}, Ты долбаёб?")
 
 @bot.message_handler(func=lambda message: message.text.lower() == 'привет')
 def send_hello(message):
@@ -20,10 +33,37 @@ def send_hello(message):
 def delete_shorts(message):
 	current_date = datetime.datetime.now()
 	user_name = message.from_user.username
-	
+
 	bot.delete_message(message.chat.id, message.message_id)
 	print(current_date, f' {user_name} Удалён шортс - ', message.text)
-	bot.send_message(message.chat.id, 'ПОПЫТКА СКИНУТЬ ШОРТС ПРЕДОТВРАЩЕНА')
+	bot.send_message(message.chat.id, 'УБЕРИ ЭТУ ХУЙНЮ')
 
-print('Бот запущен')
+@bot.message_handler(func=lambda message: edit_text in message.text)
+def delete_shorts(message):
+	current_date = datetime.datetime.now()
+	user_name = message.from_user.username
+
+	bot.delete_message(message.chat.id, message.message_id)
+	print(current_date, f' {user_name} Удалён шортс - ', message.text)
+	bot.send_message(message.chat.id, 'УБЕРИ ЭТУ ХУЙНЮ')
+
+@bot.edited_message_handler(func=lambda message: enemy_text in message.text)
+def delete_edited_shorts(message):
+	current_date = datetime.datetime.now()
+	user_name = message.from_user.username
+
+	bot.delete_message(message.chat.id, message.message_id)
+	print(current_date, f' {user_name} Удалён шортс - ', message.text)
+	bot.send_message(message.chat.id, 'УБЕРИ ЭТУ ХУЙНЮ')
+
+@bot.edited_message_handler(func=lambda message: edit_text in message.text)
+def delete_edited_shorts(message):
+	current_date = datetime.datetime.now()
+	user_name = message.from_user.username
+
+	bot.delete_message(message.chat.id, message.message_id)
+	print(current_date, f' {user_name} Удалён шортс - ', message.text)
+	bot.send_message(message.chat.id, 'УБЕРИ ЭТУ ХУЙНЮ')
+
+print('Бот запущен\n')
 bot.polling()
